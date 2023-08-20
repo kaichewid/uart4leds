232291e2c5358a310dcd5f7ed4e09f210b374894
Kai Feng Chew <kafechew@gmail.com>
2023-08-20 20:48:26 +0800

    Initial commit

---
+++ build/image_preload.cmake
@@ -0,0 +1,7 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf9160dk_nrf9160_ns" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "/Users/kafechew/Nordic/play/lesson5/fund_less5_exer1_solution/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "/usr/local/opt/python@3.9/bin/python3.9" CACHE INTERNAL "NCS child image controlled")
---
+++ build/zephyr/zephyr.dts.d
@@ -0,0 +1,24 @@
+empty_file.o: /Users/kafechew/Nordic/ncs/zephyr/misc/empty_file.c \
+ /Users/kafechew/Nordic/ncs/zephyr/boards/arm/nrf9160dk_nrf9160/nrf9160dk_nrf9160_ns.dts \
+ /Users/kafechew/Nordic/ncs/zephyr/dts/arm/nordic/nrf9160ns_sica.dtsi \
+ /Users/kafechew/Nordic/ncs/zephyr/dts/common/mem.h \
+ /Users/kafechew/Nordic/ncs/zephyr/dts/arm/nordic/nrf91ns.dtsi \
+ /Users/kafechew/Nordic/ncs/zephyr/dts/arm/armv8-m.dtsi \
+ /Users/kafechew/Nordic/ncs/zephyr/dts/common/skeleton.dtsi \
+ /Users/kafechew/Nordic/ncs/zephyr/dts/arm/nordic/nrf_common.dtsi \
+ /Users/kafechew/Nordic/ncs/zephyr/include/zephyr/dt-bindings/adc/adc.h \
+ /Users/kafechew/Nordic/ncs/zephyr/include/zephyr/dt-bindings/adc/nrf-adc.h \
+ /Users/kafechew/Nordic/ncs/zephyr/include/zephyr/dt-bindings/dt-util.h \
+ /Users/kafechew/Nordic/ncs/zephyr/include/zephyr/sys/util_macro.h \
+ /Users/kafechew/Nordic/ncs/zephyr/include/zephyr/sys/util_internal.h \
+ /Users/kafechew/Nordic/ncs/zephyr/include/zephyr/sys/util_loops.h \
+ /Users/kafechew/Nordic/ncs/zephyr/include/zephyr/dt-bindings/gpio/gpio.h \
+ /Users/kafechew/Nordic/ncs/zephyr/include/zephyr/dt-bindings/i2c/i2c.h \
+ /Users/kafechew/Nordic/ncs/zephyr/include/zephyr/dt-bindings/pinctrl/nrf-pinctrl.h \
+ /Users/kafechew/Nordic/ncs/zephyr/include/zephyr/dt-bindings/pwm/pwm.h \
+ /Users/kafechew/Nordic/ncs/zephyr/dts/common/freq.h \
+ /Users/kafechew/Nordic/ncs/zephyr/dts/arm/nordic/override.dtsi \
+ /Users/kafechew/Nordic/ncs/zephyr/dts/arm/nordic/nrf91_peripherals.dtsi \
+ /Users/kafechew/Nordic/ncs/zephyr/boards/arm/nrf9160dk_nrf9160/nrf9160dk_nrf9160_common.dts \
+ /Users/kafechew/Nordic/ncs/zephyr/boards/arm/nrf9160dk_nrf9160/nrf9160dk_nrf9160_common-pinctrl.dtsi \
+ /Users/kafechew/Nordic/ncs/zephyr/boards/arm/nrf9160dk_nrf9160/nrf9160dk_nrf9160_partition_conf.dts
